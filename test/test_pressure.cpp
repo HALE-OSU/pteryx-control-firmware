8a9ad2ff8b38c4b30d75bfbab65b0d7531a97909
LucaHaverty <lucahaverty@gmail.com>
2026-08-10 22:02:51 -0700

    Removed example failing test

--- test/test_pressure.cpp
+++ test/test_pressure.cpp
@@ -10,13 +10,8 @@ void example_passing_test(void) {
     TEST_ASSERT_EQUAL(0, analogInputToVoltage(0));
 }
 
-void example_failing_test(void) {
-    TEST_ASSERT_EQUAL(12, analogInputToVoltage(2));
-}
-
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(example_passing_test);
-    RUN_TEST(example_failing_test);
     return UNITY_END();
 }
