9016ae0d37ad22fb8abbbe731445b34ebc82f041
LucaHaverty <lucahaverty@gmail.com>
2026-08-11 10:27:58 -0700

    Fixed teensy environment building

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #if defined(ENV_TEENSY)
 #include <Arduino.h>
-#include "pt.h"
 #include "modules/solenoid_module.h"
 
-void setup() {
-    analogInputToVoltage(0);
-}
+void setup() {}
 
 void loop() {}
 #endif
