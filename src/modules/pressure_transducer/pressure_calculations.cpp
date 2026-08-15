a02fb58ed9614759ff3d262a5d987360dafef766
LucaHaverty <lucahaverty@gmail.com>
2026-08-15 12:39:28 -0700

    Pressure calculations template

---
+++ src/modules/pressure_transducer/pressure_calculations.cpp
@@ -0,0 +1,11 @@
+#include "pressure_calculations.h"
+
+float PressureCalculations::analogInputToVoltage(int input) {
+    // Write your function implementations in this file
+}
+
+float PressureCalculations::voltageToCurrent(float voltage, float resistance) {}
+
+float PressureCalculations::currentToPressure(float current,
+                                              float linearCalibration,
+                                              float constantCalibration) {}
